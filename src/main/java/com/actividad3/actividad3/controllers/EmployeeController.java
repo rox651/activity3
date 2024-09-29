47bbc1b7a0a23af590331631bc59748e01d2b3ef
homero <homerojose69@gmail.com>
2024-09-28 23:05:07 -0500

    feat: add routes for controller

--- src/main/java/com/actividad3/actividad3/controllers/EmployeeController.java
+++ src/main/java/com/actividad3/actividad3/controllers/EmployeeController.java
@@ -1,16 +1,11 @@
 package com.actividad3.actividad3.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.actividad3.actividad3.models.EmployeeEntity;
@@ -24,7 +19,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
+    @GetMapping("/")
     public String getAllEmployees(Model model) {
         Iterable<EmployeeEntity> employees = employeeService.getAllEmployees();
 
@@ -33,7 +28,7 @@ public class EmployeeController {
         return "list-employees";
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/employees/{id}")
     public String getEmployeeById(@PathVariable Long id, Model model) {
         EmployeeEntity employee = employeeService.getEmployeeById(id);
 
@@ -42,34 +37,45 @@ public class EmployeeController {
         return "employee";
     }
 
-    // @PostMapping("/create-employee")
-    // public ResponseEntity<EmployeeEntity> createEmployee(@RequestBody
-    // EmployeeEntity employee) {
-    // EmployeeEntity newEmployee = employeeService.createEmployee(employee);
-    // return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
-    // }
+    @GetMapping("/employees/{id}/edit")
+    public String editEmployee(@PathVariable Long id, Model model) {
+        EmployeeEntity employee = employeeService.getEmployeeById(id);
 
-    // // // Actualizar un producto
-    // @PutMapping("/{id}")
-    // public ResponseEntity<EmployeeEntity> updateEmployee(@PathVariable Long id,
-    // @RequestBody EmployeeEntity employee) {
-    // EmployeeEntity newEmployee = employeeService.getEmployeeById(id);
-    // if (newEmployee != null) {
-    // newEmployee.setFirstName(employee.getFirstName());
-    // newEmployee.setLastName(employee.getLastName());
-    // newEmployee.setEmail(employee.getEmail());
-    // return new ResponseEntity<>(employeeService.updateEmployee(id, newEmployee),
-    // HttpStatus.OK);
-    // } else {
-    // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    // }
+        model.addAttribute("employee", employee);
 
-    // }
+        return "edit-employee";
+    }
 
-    // @DeleteMapping("/{id}")
-    // public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
-    // employeeService.deleteEmployee(id);
-    // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    // }
+    @GetMapping("/employees/{id}/delete")
+    public String removeEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/add-employee")
+    public String addEmployee(Model model) {
+        model.addAttribute("newEmployee", new EmployeeEntity(null, null, null));
+        return "add-employee";
+    }
+
+    @PostMapping("/create-employee")
+    public String createEmployee(@ModelAttribute EmployeeEntity newEmployee) {
+        employeeService.createEmployee(newEmployee);
+        return "redirect:/";
+    }
+
+    @PostMapping("/update-employee/{id}")
+    public String updateEmployee(@PathVariable Long id,
+            @ModelAttribute EmployeeEntity employee) {
+
+        EmployeeEntity newEmployee = employeeService.getEmployeeById(id);
+        newEmployee.setFirstName(employee.getFirstName());
+        newEmployee.setLastName(employee.getLastName());
+        newEmployee.setEmail(employee.getEmail());
+
+        employeeService.updateEmployee(id, newEmployee);
+
+        return "redirect:/";
+    }
 
 }
